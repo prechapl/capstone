08c325bb2860505827f1e234b638e7824cb6d2e4
Ruby Barry <rubypbarry@gmail.com>
2019-05-27 16:57:27 -0400

    fixed family router

--- api/families.js
+++ api/families.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const { Family } = require('../db/models');
 
 //get all families
-router.get('/families', (req, res, next) => {
+router.get('/', (req, res, next) => {
     Family.findAll()
         .then(families => res.send(families))
         .catch(next)
 });
 
 //get family by id
-router.get('/families/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     Family.findByPk(req.params.id)
         .then(family => res.send(family))
         .catch(next)
