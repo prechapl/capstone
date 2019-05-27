a3b0ad96317c2efd636d3a02980850d3a622f202
Dave <dave.e.siegel@gmail.com>
2019-05-27 16:27:01 -0400

    mood model and tests

---
+++ db/models/Mood.js
@@ -0,0 +1,43 @@
+const db = require('../db');
+const { Sequelize } = db;
+
+const Mood = db.define(
+  'mood',
+  {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Mood must be named.',
+        },
+      },
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: function(mood) {
+        return Mood.findOne({
+          where: { userId: mood.userId, active: true },
+        }).then(foundMood => {
+          if (foundMood) {
+            foundMood.update({ active: false });
+          }
+        });
+      },
+    },
+  }
+);
+
+module.exports = Mood;
--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,10 @@
-const User = require('./User')
-const Family = require('./Family')
-const Relationship = require('./Relationship')
+const User = require('./User');
+const Family = require('./Family');
+const Relationship = require('./Relationship');
 const Poll = require('./Poll');
 const Choice = require('./Choice');
 const Vote = require('./Vote');
+const Mood = require('./Mood');
 
 Poll.belongsTo(User, { as: 'Owner', foreignKey: 'ownerId' });
 User.hasMany(Poll);
@@ -18,9 +19,12 @@ User.hasMany(Vote);
 Vote.belongsTo(Poll);
 Poll.hasMany(Vote);
 
-Family.hasMany(User)
-User.belongsTo(Family)
+Family.hasMany(User);
+User.belongsTo(Family);
 
-User.belongsToMany(User, { as: 'Relationship', through: Relationship })
+User.belongsToMany(User, { as: 'Relationship', through: Relationship });
 
-module.exports = { User, Family, Relationship, Poll, Choice, Vote }
+Mood.belongsTo(User);
+User.hasMany(Mood);
+
+module.exports = { User, Family, Relationship, Poll, Choice, Vote, Mood };
