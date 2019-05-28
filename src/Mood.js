b9fd16f04d12b5695b26085171ba8c8dc652521f
Preston Chaplin <preston.chaplin@gmail.com>
2019-05-28 18:23:56 -0400

    working on mood component

--- src/Mood.js
+++ src/Mood.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Avatar, Button, Slider } from 'react-native-elements';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Avatar, Button, Slider } from "react-native-elements";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   avatar: {
     borderWidth: 1
   },
   col: {
-    flexDirection: 'column',
-    alignItems: 'center'
+    flexDirection: "column",
+    alignItems: "center"
   },
   fitButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
 
@@ -32,36 +32,35 @@ export default class Mood extends React.Component {
   }
   render() {
     const { navigation } = this.props;
-    const userTitle = navigation.getParam('firstName', 'no name');
-    const url = navigation.getParam('imgUrl', 'no url');
+    const userTitle = navigation.getParam("firstName", "no name");
+    const url = navigation.getParam("imgUrl", "no url");
+    console.log("in mood render", this.props.navigation);
 
     return (
-      <View style={styles.container}>
-        <View style={{ flexDirection: 'column' }}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'stretch',
-              justifyContent: 'center'
-            }}
-          >
-            <Slider
-              value={this.state.mood}
-              onValueChange={value => this.setState({ mood: value })}
-            />
-          </View>
-          <View style={{ flexDirection: 'row' }}>
-            <Text>Moodometer: {this.state.mood}</Text>
-          </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          backgroundColor: "#fff",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "stretch",
+            justifyContent: "center"
+          }}
+        >
+          <Text>Mood Meter: {this.state.mood}</Text>
+          <Slider
+            value={this.state.mood}
+            onValueChange={value => this.setState({ mood: value })}
+          />
         </View>
-        {/* <View style={styles.fitButton}>
-            <Button
-              title="Mood"
-              onPress={() => this.props.navigation.navigate('Mood')}
-              buttonStyle={{ backgroundColor: '#FF9900', margin: 24 }}
-            />
-          </View> */}
-        <View>
+
+        <View style={{ flex: 1, flexDirection: "row", marginTop: 5 }}>
           <Avatar
             rounded
             overlayContainerStyle={styles.avatar}
@@ -75,20 +74,20 @@ export default class Mood extends React.Component {
         <View style={styles.row}>
           <Button
             title="Family"
-            onPress={() => this.props.navigation.navigate('Family')}
-            buttonStyle={{ backgroundColor: '#8EB51A', margin: 24 }}
+            onPress={() => this.props.navigation.navigate("Family")}
+            buttonStyle={{ backgroundColor: "#8EB51A", margin: 24 }}
           />
 
           <Button
             title="Values"
-            onPress={() => this.props.navigation.navigate('Values')}
-            buttonStyle={{ backgroundColor: '#7DC6CD', margin: 24 }}
+            onPress={() => this.props.navigation.navigate("Values")}
+            buttonStyle={{ backgroundColor: "#7DC6CD", margin: 24 }}
           />
 
           <Button
             title="Events"
-            onPress={() => this.props.navigation.navigate('Events')}
-            buttonStyle={{ backgroundColor: '#EF5029', margin: 24 }}
+            onPress={() => this.props.navigation.navigate("Events")}
+            buttonStyle={{ backgroundColor: "#EF5029", margin: 24 }}
           />
         </View>
       </View>
