3214d278dd34553aaabc0bae71df920438ef21e9
Preston Chaplin <preston.chaplin@gmail.com>
2019-05-31 00:47:02 -0400

    working on nav

--- src/Nav.js
+++ src/Nav.js
@@ -2,26 +2,33 @@ import {
   createBottomTabNavigator,
   createStackNavigator,
   createAppContainer
-} from 'react-navigation';
-import Family from './Family';
-import User from './User';
-import Login from './Login';
-import SignUp from './SignUp';
-import Mood from './Mood';
-import AllPolls from './AllPolls';
-import SinglePoll from './SinglePoll';
+} from "react-navigation";
+import Family from "./Family";
+import User from "./User";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Mood from "./Mood";
+import AllPolls from "./AllPolls";
+import SinglePoll from "./SinglePoll";
 
-const AppNavigator = createBottomTabNavigator({
-  // const AppNavigator = createStackNavigator({
-  'Log In': Login,
-  'Sign Up': SignUp,
-  Family: Family,
-  User: User,
-  Mood: Mood,
+const PollsNavigator = createStackNavigator({
   Polls: AllPolls,
   Poll: SinglePoll
 });
 
-const AppContainer = createAppContainer(AppNavigator);
+const AuthNavigator = createStackNavigator({
+  "Log In": Login,
+  "Sign Up": SignUp
+});
+
+const RootNavigator = createBottomTabNavigator({
+  Account: AuthNavigator,
+  User: User,
+  Family: Family,
+  Mood: Mood,
+  Polls: PollsNavigator
+});
+
+const AppContainer = createAppContainer(RootNavigator);
 
 export default AppContainer;
