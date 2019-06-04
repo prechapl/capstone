177b585375c50a2fe9fb6050e98e4f7be7863126
Lauren West <laurenwest12@gmail.com>
2019-06-03 22:44:58 -0400

    added polls to pull data from store

--- src/SinglePoll.js
+++ src/SinglePoll.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity
+} from 'react-native';
 import { RadioButtons } from 'react-native-radio-buttons';
-import { withNavigation } from 'react-navigation';
+import { connect } from 'react-redux';
+import { fetchChoices, fetchVotes, castVoteThunk } from './store/polls';
+import { findChoiceText } from './HelperFunctions';
+import PureChart from 'react-native-pure-chart';
 
 const styles = StyleSheet.create({
   container: {
@@ -31,29 +40,80 @@ const styles = StyleSheet.create({
     padding: 10,
     width: 300,
     borderWidth: 1
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF'
   }
 });
 
 class SinglePoll extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      userId: '1544f466-8518-4b8d-91ed-f5f9660eee85',
+      pollId: props.navigation.getParam('id'),
+      choiceId: '',
       selectedOption: ''
     };
   }
+  componentDidMount() {
+    this.props.fetchChoices(this.state.pollId);
+    this.props.fetchVotes(this.state.pollId);
+  }
+
+  handleSubmit = () => {
+    this.props.castVote(this.state.pollId, this.state);
+  };
 
   render() {
-    const options = ['Chicken', 'Steak'];
+    const { choices, votes } = this.props;
+
+    const options = choices.reduce((acc, item) => {
+      acc.push(item.text);
+      return acc;
+    }, []);
+
+    const votesObj =
+      votes.length &&
+      votes.reduce((acc, item) => {
+        if (!acc[item.choiceId]) {
+          acc[item.choiceId] = 1;
+        } else {
+          acc[item.choiceId]++;
+        }
+        return acc;
+      }, {});
+
+    const keys = Object.keys(votesObj);
+
+    const votesData =
+      keys.length &&
+      keys.reduce((acc, item) => {
+        const label = choices.length && findChoiceText(item, choices);
+        const obj = {};
+        obj.value = votesObj[item];
+        obj.label = label;
+        acc.push(obj);
+        return acc;
+      }, []);
 
     const question = this.props.navigation.getParam('question', 'no question');
 
-    function setSelectedOption(selectedOption) {
-      this.setState({
-        selectedOption
-      });
-    }
+    const usersWithVotes = votes.reduce((acc, vote) => {
+      acc.push(vote.userId);
+      return acc;
+    }, []);
 
-    function renderOption(option, selected, onSelect, index) {
+    const setSelectedOption = selectedOption => {
+      const selected = choices.filter(choice => choice.text === selectedOption);
+      this.setState({
+        selectedOption,
+        choiceId: selected[0].id
+      });
+    };
+
+    const renderOption = (option, selected, onSelect, index) => {
       const selectedStyle = styles.selected;
       const unselectedStyle = styles.unselected;
       const style = selected ? selectedStyle : unselectedStyle;
@@ -63,25 +123,65 @@ class SinglePoll extends React.Component {
           <Text style={style}>{option}</Text>
         </TouchableWithoutFeedback>
       );
-    }
+    };
 
     function renderContainer(optionNodes) {
       return <View>{optionNodes}</View>;
     }
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>{question}</Text>
-        <RadioButtons
-          options={options}
-          onSelection={setSelectedOption.bind(this)}
-          selectedOption={this.state.selectedOption}
-          renderOption={renderOption}
-          renderContainer={renderContainer}
-        />
-      </View>
-    );
+    if (usersWithVotes.includes(this.state.userId)) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.header}>{question}</Text>
+          <PureChart data={votesData} type="pie" />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.header}>{question}</Text>
+
+          <RadioButtons
+            options={options}
+            onSelection={setSelectedOption.bind(this)}
+            selectedOption={this.state.selectedOption}
+            renderOption={renderOption}
+            renderContainer={renderContainer}
+          />
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#8EB51A',
+              padding: 10,
+              margin: 10,
+              width: 300
+            }}
+            onPress={this.handleSubmit}
+          >
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
   }
 }
 
-export default withNavigation(SinglePoll);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchChoices: id => dispatch(fetchChoices(id)),
+    fetchVotes: id => dispatch(fetchVotes(id)),
+    castVote: (id, vote) => dispatch(castVoteThunk(id, vote))
+  };
+};
+
+const mapStateToProps = ({ choices, votes }) => {
+  return {
+    choices,
+    votes
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SinglePoll);
