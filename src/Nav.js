c1e1a26db8bc34b5ab1d170ab15f2a97b6e04074
Preston Chaplin <preston.chaplin@gmail.com>
2019-06-07 14:42:00 -0400

    added navigation animation config to usersNavigator

--- src/Nav.js
+++ src/Nav.js
@@ -41,33 +41,15 @@ const UserNavigator = createStackNavigator(
     }
   },
   {
-    headerMode: "none",
-    mode: "modal",
-    defaultNavigationOptions: {
+    NavigationOptions: {
       gesturesEnabled: false
     },
+
     transitionConfig: () => ({
       transitionSpec: {
-        duration: 300,
+        duration: 0,
         easing: Easing.out(Easing.poly(4)),
         timing: Animated.timing
-      },
-      screenInterpolator: sceneProps => {
-        const { layout, position, scene } = sceneProps;
-        const { index } = scene;
-
-        const height = layout.initHeight;
-        const translateY = position.interpolate({
-          inputRange: [index - 1, index, index + 1],
-          outputRange: [height, 0, 0]
-        });
-
-        const opacity = position.interpolate({
-          inputRange: [index - 1, index - 0.99, index],
-          outputRange: [0, 1, 1]
-        });
-
-        return { opacity, transform: [{ translateY }] };
       }
     })
   }
