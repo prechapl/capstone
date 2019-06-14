df03c8429eb5f15281f3f6a8e53b3a46a18e32f1
Preston Chaplin <preston.chaplin@gmail.com>
2019-06-14 19:44:15 -0400

    added location to Nav system

--- src/AvatarGenerator.js
+++ src/AvatarGenerator.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { StyleSheet, Text, View } from "react-native";
-import { Avatar } from "react-native-elements";
-import { withNavigation } from "react-navigation";
-import ActionButton from "react-native-circular-action-menu";
-import { findMoodColor } from "./HelperFunctions";
-import AllPolls from "./Polls/AllPolls";
-import Events from "./Events/Events";
-import Mood from "./Mood";
-import Family from "./Family";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { StyleSheet, Text, View } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { withNavigation } from 'react-navigation';
+import ActionButton from 'react-native-circular-action-menu';
+import { findMoodColor } from './HelperFunctions';
+import AllPolls from './Polls/AllPolls';
+import Events from './Events/Events';
+import Mood from './Mood';
+import Family from './Family';
+import Location from './Location';
 
 class AvatarGenerator extends Component {
   constructor(props) {
@@ -25,10 +26,10 @@ class AvatarGenerator extends Component {
 
   render() {
     const { navigation } = this.props;
-    const user = navigation.getParam("user");
-    const mood = navigation.getParam("mood");
-    const buttonSet = navigation.getParam("buttonSet");
-    const componentToNest = navigation.getParam("nestComponent");
+    const user = navigation.getParam('user');
+    const mood = navigation.getParam('mood');
+    const buttonSet = navigation.getParam('buttonSet');
+    const componentToNest = navigation.getParam('nestComponent');
     const moodColor = findMoodColor(mood.value);
     // const moodText = findMoodText(mood.value);
 
@@ -36,20 +37,20 @@ class AvatarGenerator extends Component {
       <View
         style={{
           flex: 1,
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center"
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
         }}
       >
-        <View style={{ flexDirection: "row", marginBottom: 60 }}>
+        <View style={{ flexDirection: 'row', marginBottom: 60 }}>
           {componentToNest}
         </View>
 
         <View
           style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
             paddingEnd: 25,
             marginBottom: 25
           }}
@@ -78,30 +79,8 @@ class AvatarGenerator extends Component {
             }
           >
             {buttons[buttonSet].map((button, idx) => {
-              return button.title === "Family" ? (
-                <ActionButton.Item
-                  key={idx}
-                  onPress={() =>
-                    this.props.navigation.navigate(button.title, {
-                      user: user
-                    })
-                  }
-                >
-                  <View
-                    style={{
-                      width: button.width,
-                      height: button.height,
-                      backgroundColor: button.color,
-                      borderRadius: 40,
-                      position: "relative",
-                      paddingStart: 14,
-                      paddingTop: 13
-                    }}
-                  >
-                    <Text style={styles.text}>{button.title}</Text>
-                  </View>
-                </ActionButton.Item>
-              ) : (
+              return button.title !== 'Location' &&
+                button.title !== 'Family' ? (
                 <ActionButton.Item
                   key={idx}
                   onPress={() =>
@@ -113,10 +92,34 @@ class AvatarGenerator extends Component {
                   <View
                     style={{
                       width: button.width,
+                      height: button.height,
+                      backgroundColor: button.color,
+                      borderRadius: 40,
+                      position: 'relative',
+                      paddingStart: 14,
+                      paddingTop: 13
+                    }}
+                  >
+                    <Text style={styles.text}>{button.title}</Text>
+                  </View>
+                </ActionButton.Item>
+              ) : (
+                <ActionButton.Item
+                  key={idx}
+                  onPress={() =>
+                    this.props.navigation.navigate(button.title, {
+                      user: user,
+                      mood: mood
+                    })
+                  }
+                >
+                  <View
+                    style={{
+                      width: button.width,
                       backgroundColor: button.color,
                       height: button.height,
                       borderRadius: 40,
-                      position: "relative",
+                      position: 'relative',
                       paddingStart: 14,
                       paddingTop: 12
                     }}
@@ -136,97 +139,111 @@ class AvatarGenerator extends Component {
 const buttons = {
   UserButtons: [
     {
-      title: "Mood",
-      color: "#FF9900",
+      title: 'Mood',
+      color: '#FF9900',
       width: 80,
       height: 50,
       componentToNest: <Mood />
     },
     {
-      title: "Family",
-      color: "#8EB51A",
+      title: 'Family',
+      color: '#8EB51A',
       width: 83,
       height: 50,
       componentToNest: <Family />
     },
     {
-      title: "Events",
-      color: "#EF5029",
+      title: 'Polls',
+      color: '#7DC6CD',
+      width: 70,
+      height: 50,
+      componentToNest: <AllPolls />
+    },
+    {
+      title: 'Events',
+      color: '#EF5029',
       width: 87,
       height: 50,
       componentToNest: <Events />
     },
     {
-      title: "Polls",
-      color: "#7DC6CD",
-      width: 70,
+      title: 'Location',
+      color: '#AD0978',
+      width: 99,
       height: 50,
-      componentToNest: <AllPolls />
+      componentToNest: <Location />
     }
   ],
   RelativeButtons: [
     {
-      title: "Family",
-      color: "#8EB51A",
+      title: 'Family',
+      color: '#8EB51A',
       width: 83,
       height: 50,
       componentToNest: <Family />
     },
     {
-      title: "Events",
-      color: "#EF5029",
+      title: 'Events',
+      color: '#EF5029',
       width: 87,
       height: 50,
       componentToNest: <Events />
     },
     {
-      title: "Polls",
-      color: "#7DC6CD",
+      title: 'Polls',
+      color: '#7DC6CD',
       width: 70,
       height: 50,
       componentToNest: <AllPolls />
+    },
+    {
+      title: 'Location',
+      color: '#AD0978',
+      width: 99,
+      height: 50,
+      componentToNest: <Location />
     }
   ],
   ChildButtons: [
     {
-      title: "Location",
-      color: "#AD0978",
+      title: 'Location',
+      color: '#AD0978',
       width: 99,
       height: 50,
-      componentToNest: null
+      componentToNest: <Location />
     },
 
     {
-      title: "Events",
-      color: "#EF5029",
+      title: 'Events',
+      color: '#EF5029',
       width: 87,
       height: 50,
       componentToNest: <Events />
     },
     {
-      title: "Polls",
-      color: "#7DC6CD",
+      title: 'Polls',
+      color: '#7DC6CD',
       width: 70,
       height: 50,
       componentToNest: <AllPolls />
     },
     {
-      title: "Family",
-      color: "#8EB51A",
+      title: 'Family',
+      color: '#8EB51A',
       width: 83,
       height: 50,
       componentToNest: <Family />
     },
     {
-      title: "Goals",
-      color: "#1500FA",
+      title: 'Goals',
+      color: '#1500FA',
       width: 78,
       height: 50,
       componentToNest: null
     },
     {
-      title: "Records",
-      color: "#E0BF00",
+      title: 'Awards',
+      color: '#E0BF00',
       width: 98,
       height: 50,
       componentToNest: null
@@ -236,7 +253,7 @@ const buttons = {
 
 const styles = StyleSheet.create({
   text: {
-    color: "white",
+    color: 'white',
     fontSize: 20
   }
 });
---
+++ src/Location.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { withNavigation } from 'react-navigation';
+import { Alert, View } from 'react-native';
+import { MapView } from 'expo';
+import { Marker } from 'react-native-maps';
+import { Avatar } from 'react-native-elements';
+import { findMoodColor } from './HelperFunctions';
+
+class Location extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: { coords: null }
+    };
+  }
+
+  componentDidMount() {
+    this.findCoordinates();
+  }
+
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const loc = position;
+        this.setState({ location: loc });
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 30000, maximumAge: 5000 }
+    );
+  };
+
+  render() {
+    const user = this.props.navigation.getParam('user');
+    const mood = this.props.navigation.getParam('mood');
+    const locate = this.state.location;
+    if (locate.coords !== null) {
+      const accuracy = locate.coords.accuracy;
+      const latitude = locate.coords.latitude;
+      const longitude = locate.coords.longitude;
+      const oneDegreeOfLatitudeInMeters = 111.32 * 1000;
+      const latDelta = accuracy / oneDegreeOfLatitudeInMeters;
+      const longDelta =
+        accuracy /
+        (oneDegreeOfLatitudeInMeters * Math.cos(latitude * (Math.PI / 180)));
+      console.log('latitude', locate.coords.latitude);
+      console.log('longitude', locate.coords.longitude);
+      console.log('latitudeDelta', latDelta);
+      console.log('longitudeDelta', longDelta);
+
+      return (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: latitude,
+            longitude: locate.coords.longitude,
+            latitudeDelta: latDelta,
+            longitudeDelta: longDelta
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: latitude,
+              longitude: longitude
+            }}
+            title="username"
+            description="location of user"
+          >
+            <View>
+              <Avatar
+                rounded
+                overlayContainerStyle={{
+                  borderWidth: 7,
+                  borderColor: findMoodColor(mood.value)
+                }}
+                size={50}
+                source={{
+                  uri: `${user.imgUrl}`
+                }}
+                title={user.firstName}
+              />
+            </View>
+          </Marker>
+        </MapView>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default withNavigation(Location);
