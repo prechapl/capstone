755a29d2a2b8b880633588c53a30d7dfb6a6e02a
Preston Chaplin <preston.chaplin@gmail.com>
2019-06-15 00:32:18 -0400

    working on profile image picker

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,33 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView,
-} from 'react-native';
-import { signUp, getAuthedUser } from './store/users';
-import console from 'console';
+  KeyboardAvoidingView
+} from "react-native";
+import { signUp, getAuthedUser } from "./store/users";
+import { ImagePicker, Permissions, Constants } from "expo";
+import { Avatar } from "react-native-elements";
 
 class SignUp extends Component {
   constructor() {
     super();
 
     this.state = {
-      firstName: '',
-      lastName: '',
-      age: '',
-      email: '',
-      password: '',
-      imgUrl: '',
-      familyCode: '',
-      newFamilyCode: '',
-      newFamilyName: '',
+      firstName: "",
+      lastName: "",
+      age: "",
+      email: "",
+      password: "",
+      imgUrl: "",
+      familyCode: "",
+      newFamilyCode: "",
+      newFamilyName: "",
       page: 1,
+      imageUri: null,
+      imageBase64: null
     };
   }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true
+    });
+
+    if (!result.cancelled) {
+      this.setState({ imageUri: result.uri, imageBase64: result.base64 });
+    }
+  };
 
   handleSubmit = userData => {
     return signUp(userData)
@@ -43,10 +72,10 @@ class SignUp extends Component {
       password: this.state.password,
       age: this.state.age,
       imgUrl: this.state.imgUrl,
-      familyCode: this.state.familyCode,
+      familyCode: this.state.familyCode
     };
     this.handleSubmit(userData).then(() =>
-      this.props.navigation.navigate('SetAllRelationships')
+      this.props.navigation.navigate("SetAllRelationships")
     );
   };
 
@@ -60,35 +89,79 @@ class SignUp extends Component {
       imgUrl: this.state.imgUrl,
       family: {
         code: this.state.newFamilyCode,
-        name: this.state.newFamilyName,
-      },
+        name: this.state.newFamilyName
+      }
     };
     this.handleSubmit(userData).then(() =>
-      this.props.navigation.navigate('Family')
+      this.props.navigation.navigate("Family")
     );
   };
 
   nextPage = () => {
     this.setState({
-      page: this.state.page + 1,
+      page: this.state.page + 1
     });
   };
 
   previousPage = () => {
     this.setState({
-      page: this.state.page - 1,
+      page: this.state.page - 1
     });
   };
 
   render() {
-    if (this.state.page === 1) {
+    if (this.state.page === 1 && this.state.imageBase64) {
+      let { imageUri, imageBase64 } = this.state;
+      // let _imageUriprepend = "data:image/" + imageUri.slice(-3) + ";base64,";
+      // let uri = _imageUriprepend.concat(imageBase64);
+      // console.log("uri", uri);
+      console.log("imageUri.slice(-3)", imageUri.slice(-3));
       return (
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
           <View style={styles.container}>
-            <Text style={styles.header}>Mender</Text>
-            <Text style={{ fontSize: 12, marginBottom: 16 }}>
-              sign up for a free account
-            </Text>
+            {imageBase64 ? (
+              <View>
+                <Avatar
+                  rounded
+                  editButton="true"
+                  onEditPress={this._pickImage}
+                  overlayContainerStyle={{
+                    borderWidth: 7,
+                    borderColor: "#009510"
+                  }}
+                  size={120}
+                  source={{
+                    uri: _imageUri
+                  }}
+                  // title={this.state.firstName}
+                />
+              </View>
+            ) : (
+              <View>
+                <Avatar
+                  rounded
+                  editButton="true"
+                  onEditPress={this._pickImage}
+                  overlayContainerStyle={{
+                    borderWidth: 7,
+                    borderColor: "#009510"
+                  }}
+                  source={{}}
+                  size={120}
+                />
+                <Text style={styles.header}>Mender</Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    marginBottom: 16,
+                    alignItems: "center"
+                  }}
+                >
+                  sign up for a free account
+                </Text>
+              </View>
+            )}
+
             <TextInput
               value={this.state.firstName}
               style={styles.input}
@@ -110,19 +183,6 @@ class SignUp extends Component {
               onChangeText={age => this.setState({ age })}
             />
 
-            <TouchableOpacity style={styles.button} onPress={this.nextPage}>
-              <Text style={styles.buttonText}>Next</Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAvoidingView>
-      );
-    }
-
-    if (this.state.page === 2) {
-      return (
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-          <View style={styles.container}>
-            <Text style={styles.header}>Mend</Text>
             <TextInput
               value={this.state.email}
               style={styles.input}
@@ -141,46 +201,17 @@ class SignUp extends Component {
             />
 
             <TouchableOpacity style={styles.button} onPress={this.nextPage}>
-              <Text style={styles.buttonText}>Next</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={this.previousPage}>
-              <Text style={styles.buttonText}>Previous</Text>
+              <Text style={styles.buttonText}>Create or Join a Family</Text>
             </TouchableOpacity>
           </View>
         </KeyboardAvoidingView>
       );
     }
 
-    if (this.state.page === 3) {
+    if (this.state.page === 2) {
       return (
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
           <View style={styles.container}>
-            <Text style={styles.header}>Mend</Text>
-            <TextInput
-              style={styles.input}
-              value={this.state.imgUrl}
-              placeholder="Image"
-              onChangeText={imgUrl => this.setState({ imgUrl })}
-            />
-
-            <TouchableOpacity style={styles.button} onPress={this.nextPage}>
-              <Text style={styles.buttonText}>Next</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={this.previousPage}>
-              <Text style={styles.buttonText}>Previous</Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAvoidingView>
-      );
-    }
-
-    if (this.state.page === 4) {
-      return (
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-          <View style={styles.container}>
-            <Text style={styles.header}>Mend</Text>
             <Text style={styles.header2}>Join Family</Text>
             <TextInput
               value={this.state.familyCode}
@@ -225,41 +256,41 @@ class SignUp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   input: {
     height: 40,
-    backgroundColor: '#D3D3D4',
+    backgroundColor: "#D3D3D4",
     marginBottom: 20,
     width: 300,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   button: {
-    backgroundColor: '#448AE6',
+    backgroundColor: "#448AE6",
     padding: 10,
     width: 300,
-    margin: 10,
+    margin: 10
   },
   buttonText: {
-    textAlign: 'center',
-    color: '#FFFFFF',
+    textAlign: "center",
+    color: "#FFFFFF"
   },
   header: {
     padding: 10,
     marginBottom: 30,
-    fontSize: 75,
+    fontSize: 75
   },
   header2: {
     padding: 10,
     marginBottom: 30,
-    fontSize: 42,
-  },
+    fontSize: 42
+  }
 });
 
 const mapDispatchToProps = dispatch => ({
-  getAuthedUser: () => dispatch(getAuthedUser()),
+  getAuthedUser: () => dispatch(getAuthedUser())
 });
 
 export default connect(
